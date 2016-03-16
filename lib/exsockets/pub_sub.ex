173ff530eb79484b580d5934f2e256362d4950b6
Nick Chernyshev <nick.chernyshev@gmail.com>
2016-03-16 22:25:07 +0100

    unsubscribe

--- lib/exsockets/pub_sub.ex
+++ lib/exsockets/pub_sub.ex
@@ -9,6 +9,10 @@ defmodule Exsockets.PubSub do
     GenServer.call(__MODULE__, {:subscribe, topic})
   end
 
+  def unsubscribe(topic) do
+    GenServer.call(__MODULE__, {:unsubscribe, topic})
+  end
+
   ## Callbacks
 
   def handle_call({:subscribe, topic}, {pid, _}, %{conn: conn, subs: subs} = state) do
@@ -16,6 +20,12 @@ defmodule Exsockets.PubSub do
 
     tops = Map.get(subs, pid, MapSet.new)
     subs = Map.put(subs, pid, MapSet.put(tops, topic))
+
+    {:reply, :ok, %{state | subs: subs}}
+  end
+  def handle_call({:unsubscribe, topic}, {pid, _}, %{subs: subs} = state) do
+    tops = Map.get(subs, pid, MapSet.new)
+    subs = Map.put(subs, pid, MapSet.delete(tops, topic))
 
     {:reply, :ok, %{state | subs: subs}}
   end
