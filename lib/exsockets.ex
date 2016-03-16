54edac61234e0b0a55c4853240293b64a152799f
Nick Chernyshev <nick.chernyshev@gmail.com>
2016-03-16 22:43:29 +0100

    simple server

--- lib/exsockets.ex
+++ lib/exsockets.ex
@@ -1,2 +1,30 @@
 defmodule Exsockets do
+  use Application
+
+  alias Plug.Adapters.Cowboy
+
+  def start(_, _) do
+    import Supervisor.Spec
+
+    children = [
+      Cowboy.child_spec(:http, Exsockets.Server, [], [dispatch: dispatch]),
+      worker(Exsockets.PubSub, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Exsockets.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+
+  defp dispatch do
+    [
+      {
+        :_,
+        [
+          {"/ws", Exsockets.Socket, []},
+          {:_, Cowboy.Handler, {Exsockets.Server, []}}
+        ]
+      }
+    ]
+  end
 end
